cfacad200aff5f9f1ca988132f1be2e5ca3f5fc6
Paul Lassa <plassa3@gmail.com>
2014-10-12 22:56:45 -0700

    first commit

---
+++ Twitter/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Paul L on 10/12/14.
+//  Copyright (c) 2014 Paul Lassa. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1RequestOperationManager {
+   
+}
