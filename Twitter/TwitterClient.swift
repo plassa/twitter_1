f502193df63cc4cd00b9e743037de6ce2705d724
Paul Lassa <plassa3@gmail.com>
2014-10-12 23:18:14 -0700

    Initial commit

--- Twitter/TwitterClient.swift
+++ Twitter/TwitterClient.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+let TwitterConsumerKey = "fMAYHqQo3C9Uj5CYtNaiJifaw"
+let TwitterConsumerSecret = "QiSWCPWJ0o1MVuV7hAge1IgeSYn4vlcu6ClZeIMVYzt3roS3aE"
+let TwitterBaseURL = NSURL(string: "https://api.twitter.com")
+
 class TwitterClient: BDBOAuth1RequestOperationManager {
+    
+    class var sharedInstance : TwitterClient{
+    
+    struct Static {
+        static let instance = TwitterClient(baseURL: TwitterBaseURL, consumerKey: TwitterConsumerKey, consumerSecret: TwitterConsumerSecret)
+        }
+        return Static.instance
+    }
    
 }
--- Twitter/ViewController.swift
+++ Twitter/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -20,6 +21,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onLogin(sender: AnyObject) {
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token",
+            method: "GET", callbackURL: NSURL(string: "cptwitterdemo://oauth"), scope:
+            nil, success: { (requestToken: BDBOAuthToken!) -> Void in
+            println("Got the request token")
+            var authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+            UIApplication.sharedApplication().openURL(authURL)
+                
+        })  { (error: NSError!) -> Void in
+            println("Failed to get the request token: \(error)")
+        }
 
+    }
 }
-
